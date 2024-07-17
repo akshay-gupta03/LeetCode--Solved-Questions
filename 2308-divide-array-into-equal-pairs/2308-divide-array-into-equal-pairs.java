9d5781a4bf231d34121730b0acd1af9e2d7b5ce1
akshay-gupta03 <89996003+akshay-gupta03@users.noreply.github.com>
2024-07-17 20:19:55 +0530

    Time: 1 ms (100%), Space: 44.2 MB (66.06%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+    int count[] = new int[501];
+     for(int i:nums) count[i]++;
+     for(int i:count) if(i%2!=0) return false;
+     return true;
+    }
+}
