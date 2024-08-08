2766650d13835637370def73c5e35edf73ce5476
akshay-gupta03 <89996003+akshay-gupta03@users.noreply.github.com>
2024-08-08 11:36:14 +0530

    Time: 3 ms (70.98%), Space: 42.5 MB (5.44%) - LeetHub

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+    int[] arr = new int[1001];
+    for(int i=0;i<s.length();i++){
+        arr[s.charAt(i)-'a']++;
+    } 
+    int num = arr[s.charAt(0)-'a'];
+
+    for(int i = 0; i<arr.length; i++)
+        if(num != arr[i] && arr[i] != 0)
+            return false;
+    return true;  
+    }
+}
