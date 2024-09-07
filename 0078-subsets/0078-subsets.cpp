31f8bd5292d8df0d08d0f078c7397872b9c73407
akshay-gupta03 <89996003+akshay-gupta03@users.noreply.github.com>
2024-09-07 21:09:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.76%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,15 +4,15 @@ public:
     vector<vector<int>> ans;
     vector<int> inner;
     int n=nums.size();
-    generate(nums,0,inner,ans,n);
+    answer(nums,0,inner,ans,n);
     return ans;
     }
 
-    void generate(const vector<int>& nums,int start, vector<int>& inner, vector<vector<int>>& ans, int end){
+    void answer(const vector<int>& nums,int start, vector<int>& inner, vector<vector<int>>& ans, int end){
     ans.push_back(inner);
     for(int i=start;i<end;i++){
         inner.push_back(nums[i]);
-        generate(nums,i+1,inner,ans,end);
+        answer(nums,i+1,inner,ans,end);
         inner.pop_back();
     }  
     }    
