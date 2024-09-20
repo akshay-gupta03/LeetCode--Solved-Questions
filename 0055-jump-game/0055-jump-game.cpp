b7013dc0fdde13b1030d535638b716e9966f2427
akshay-gupta03 <89996003+akshay-gupta03@users.noreply.github.com>
2024-09-20 12:01:51 +0530

    Time: 52 ms (25.83%), Space: 51.1 MB (42.42%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+    int jump=0; 
+    for(int i=0;i<nums.size();i++){
+        if(jump<0){
+            return false;
+        }
+        else if(nums[i]>jump){
+            jump=nums[i];
+        }
+        jump=jump-1;
+    }  
+    return true;
+    }
+};
