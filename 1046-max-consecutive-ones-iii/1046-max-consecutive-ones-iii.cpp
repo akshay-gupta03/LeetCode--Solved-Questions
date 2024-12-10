58a4c15f5270c391e45f5651b5f292ba755f09ff
akshay-gupta03 <89996003+akshay-gupta03@users.noreply.github.com>
2024-12-10 09:43:11 +0530

    Time: 1 ms (39.47%), Space: 59.4 MB (8.64%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,11 +5,29 @@ public:
     int i=0;
     int zeros=0;
 
+    // for(int j=0;j<nums.size();j++){
+    //     if(nums[j]==0){
+    //         zeros++;
+    //     }
+    //     while(zeros>k){
+    //         if(nums[i]==0){
+    //             zeros--;
+    //         }
+    //         i++;
+    //     }
+    //     if(zeros<=k){
+    //         int length=j-i+1;
+    //         maxlen=max(length,maxlen);
+    //     }
+    // } 
+    // return maxlen;  
+    // }
+
     for(int j=0;j<nums.size();j++){
         if(nums[j]==0){
             zeros++;
         }
-        while(zeros>k){
+        if(zeros>k){
             if(nums[i]==0){
                 zeros--;
             }
